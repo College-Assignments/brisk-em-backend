4952615f87eadf62ed01fd1d2eb0bb507fb164f2
Suraj Mandal <surajmandalcell@gmail.com>
2022-05-19 00:37:23 +0530

    workingsnapshot

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from ai import router as ai_router
+import uvicorn
 from fastapi import FastAPI
+from routes.ai import router as ai_router
 
-app = FastAPI()
 
-
-def get_application() -> FastAPI:
+def run_app() -> FastAPI:
     application = FastAPI()
-    application.include_router(ai_router, prefix="/ai")
+    print("Starting application...")
+
+    # Routes
+    application.include_router(ai_router, prefix="/api")
+
     return application
 
-@app.get("/")
-def root():
-    return {"200": "Welcome to BriskES"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:run_app", host="127.0.0.1", port=3000, log_level="info")
--- routes/ai.py
+++ routes/ai.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.post("ai")
+@router.post("/ai")
 async def generate_mcq(topic: str, topic_title: str):
-    print(topic, "\n\n", topic_title)
     return "OK"
